8f3e64d356de3a1e6f68cc1c3d3cbbc24700b13c
Joshua Baert <jbghoring@gmail.com>
2016-10-26 12:35:44 -0600

    extra page turning

--- controller.js
+++ controller.js
@@ -1,8 +1,23 @@
 angular.module('userProfiles').controller('MainController', function($scope, mainService) {
-
+	
+	$scope.prev = function () {
+		if ($scope.currentPage > 1){
+			$scope.currentPage--;
+			$scope.getUsers();
+		}
+	};
+	$scope.next = function () {
+		if ($scope.currentPage < $scope.maxPages){
+			$scope.currentPage++;
+			$scope.getUsers();
+		}
+	};
+	$scope.currentPage = 1;
+	
   $scope.getUsers = function() {
-	  mainService.getUsers().then(function (response) {
+	  mainService.getUsers($scope.currentPage).then(function (response) {
 		  $scope.users = response.data.data;
+		  $scope.maxPages = response.data.total_pages
 	  })
   };
 	
